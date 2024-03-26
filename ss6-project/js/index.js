2095ee91fafa9c72edd98f468cfd18ab0f5eba8f
auductuan <147390394+auductuan@users.noreply.github.com>
2024-03-26 08:08:57 +0700

    Add files via upload

---
+++ ss6-project/js/index.js
@@ -0,0 +1,10 @@
+/* 
+Tạo 1 mảng chứa danh sách sản phẩm
+quản lý product
+*/
+let products=[
+    {
+        image:""
+    }
+
+]
---
+++ ss6-project/js/registr.js
@@ -0,0 +1,7 @@
+// function login(){
+//     location.href="../utils/index.html"
+// }
+const all= document.getElementById("all");
+all.addEventListener("submit", function(e){
+    e.preventDefault();
+})
